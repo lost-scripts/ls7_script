7c29b5e3a5d42e7bff53e6ff33ea764c5d5e484b
Rai <rai.lopez@outlook.com>
2024-07-09 18:44:23 +0200

    Prepare ls7_script.lua for build pre-commit hook test

--- Menu/- Lost Scripts/ls7_script.lua
+++ Menu/- Lost Scripts/ls7_script.lua
@@ -1,1 +1,14 @@
-7
+-- **************************************************
+-- Provide Moho with the name of this script object
+-- **************************************************
+
+ScriptName = "LS7_Script"
+ScriptBirth = "20240414-0449"
+ScriptBuild = "20240709-1842"
+
+-- **************************************************
+-- General information about this script
+-- **************************************************
+
+LS7_Script = {}
+-- More code...
